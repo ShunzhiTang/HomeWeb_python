36cbcd21d2c0833a0fc8a492a420e7902b1d417e
ShunZhi Tang <tangshunzhi@zplay.com>
2017-10-24 15:56:35 +0800

    Create model such: User

--- ORM/Model.py
+++ ORM/Model.py
@@ -14,9 +14,28 @@ class User(Model):
     passwd = StringField(ddl='varchar(50)')
     image  = StringField(ddl='varchar(50)')
     admin = BooleanField()
-    create_at = FloatField(default=time.time())
+    created_at = FloatField(default=time.time())
 
 
 class Blog(Model):
-    
+    __table__ = 'blogs'
 
+    id = StringField(primary_key=True,default=next_id,ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(50)')
+    name = StringField(ddl='varchar(50)')
+    summary = StringField(ddl='varchar(50)')
+    content = TextField()
+    created_at = FloatField(default=time.time())
+
+class Comment(Model):
+    __table__ = 'comments'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    blog_id = StringField(ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
+    content = TextField()
+    created_at = FloatField(default=time.time)
