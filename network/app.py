453a70fdce4f4be3386a91a11de27463865ab13a
ShunZhi Tang <tangshunzhi@zplay.com>
2017-10-25 18:55:06 +0800

    Implement app initialize

--- network/app.py
+++ network/app.py
@@ -8,7 +8,8 @@ from datetime import datetime
 from  aiohttp import  web
 # jinja2 前端模板引擎jinja2：
 from jinja2 import Environment,FileSystemLoader
-from network.NetWorkHandlers import add_routes,add_static
+import database.MysqlHelper
+from network.NetWorkHandlers import add_static,add_routes
 
 # 模板初始化
 def init_jinja2(app,**kw):
@@ -54,15 +55,73 @@ async def data_factory(app,handler):
         return (await handler(request))
     return parse_data
 
+async def response_factory(app,handler):
+    async def response(request):
+        logging.info('Response handler...')
+        r = await handler(request)
+        if isinstance(r ,web.StreamResponse):
+            return r
+        if isinstance(r , bytes):
+            resp = web.Response(body=r)
+            resp.content_type = 'application/octet-stream'
+            return resp
+        if isinstance(r,str):
+            # 重定向
+            if r.startswith('redirect:'):
+                return web.HTTPFound(r[9:])
+            resp = web.Response(body=r.encode('utf-8'))
+            resp.content_type = 'text/html;charset=utf-8'
+            return resp
+        if isinstance(r,dict):
+            template = r.get('__template__')
+            if template is None:
+                resp = web.Response(body=json.dumps(r,ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
+                resp.content_type = 'application/json;charset=utf-8'
+                return resp
+            else:
+                resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
+                resp.content_type = 'text/html;charset=utf-8'
+                return resp
+        if isinstance(r,int) and r >= 100 and  r < 600:
+            return web.Response(r)
+        if isinstance(r,tuple) and len(r) == 2:
+            t,m = r
+            if isinstance(t,int) and t >= 100 and t < 600:
+                return web.Response(t, str(m))
+        resp = web.Response(body=str(r).encode('utf-8'))
+        resp.content_type = 'text/plain;charset=utf-8'
+        return resp
+    return response
 
+def datetime_filter(t):
+    delta = int(time.time() - t)
+    if delta < 60:
+        return u'1分钟前'
+    if delta < 3600:
+        return u'%s分钟前' % (delta // 60)
+    if delta < 86400:
+        return u'%s小时前' % (delta // 3600)
+    if delta < 604800:
+        return u'%s天前' % (delta // 86400)
+    dt = datetime.fromtimestamp(t)
+    return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
 
 
 def index(request):
     return web.Response(body=b'<h1>hello world</h1>',content_type='text/html')
 
 async def init(loop):
-    app= web.Application(loop = loop)
-    app.router.add_route('GET','/',index)
+
+    await database.MysqlHelper.create_pool(loop=loop,host='127.0.0.1', port=3306, user='www-data', password='www-data', database='homePage')
+    # await database.MysqlHelper.create_pool(loop=loop, user='www-data', password='www-data', database='homePage')
+    app= web.Application(loop = loop,middlewares = [
+        logger_factory,
+        response_factory
+    ])
+    init_jinja2(app ,filters=dict(datetime = datetime_filter))
+    add_routes(app,'handlers')
+    add_static(app)
+    # app.router.add_route('GET','/',index)
     srv = await loop.create_server(app.make_handler(),'127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
