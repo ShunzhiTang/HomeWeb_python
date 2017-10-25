4d321ab11c4c58c0a807bad8f7c180a205264613
ShunZhi Tang <tangshunzhi@zplay.com>
2017-10-25 18:11:51 +0800

    Implement data_factory

--- network/app.py
+++ network/app.py
@@ -32,11 +32,27 @@ def init_jinja2(app,**kw):
             env.filters[name] = f
     app['__templating__'] = env
 
+
+#  工厂模式
+
 async def logger_factory(app,handler):
     async def logger(request):
         logging.info('Request: %s %s' % (request.method, request.path))
         return (await handler(request))
     return logger
+
+async def data_factory(app,handler):
+    async def parse_data(request):
+        if request.method == 'POST':
+            # startswith  以xx前缀开头
+            if request.content_type.startswith('application/json'):
+                request.__data__ = await request.json()
+                logging.info('request json: %s' % str(request.__data__))
+            elif request.content_type.startswith('application/x-www-form-urlencoded'):
+                request.__data__ = await request.post()
+                logging.info('request form: %s' % str(request.__data__))
+        return (await handler(request))
+    return parse_data
 
 
 
