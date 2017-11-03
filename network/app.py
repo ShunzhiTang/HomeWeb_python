b33025cadcfe1e5192b3205f0e7cf2af013b8452
ShunZhi Tang <tangshunzhi@zplay.com>
2017-11-03 10:40:37 +0800

    Fixed app connection

--- network/app.py
+++ network/app.py
@@ -122,8 +122,8 @@ async def init(loop):
     add_routes(app,'handlers')
     add_static(app)
     # app.router.add_route('GET','/',index)
-    srv = await loop.create_server(app.make_handler(),'127.0.0.1', 9000)
-    logging.info('server started at http://127.0.0.1:9000...')
+    srv = await loop.create_server(app.make_handler(),'127.0.0.1', 9001)
+    logging.info('server started at http://127.0.0.1:9001...')
     return srv
 
 loop = asyncio.get_event_loop()
