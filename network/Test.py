adacb7f6de9bbe2b9a9c4269aa78ae60d7be590d
ShunZhi Tang <tangshunzhi@zplay.com>
2017-11-03 10:41:06 +0800

    Test database insert and select

--- network/Test.py
+++ network/Test.py
@@ -12,11 +12,17 @@ async def test(loop):
     userImage = 'www.person.png'
 
     user = User(name =userName,email= emailStr, passwd = '111',image = userImage,admin=False,id=userID ,created_at=time.time())
-    blog = Blog(name='新浪微博', user_id= userID , user_name= userName, user_image= userImage, id=next_id(),
-                created_at=time.time(),summary='我是一个注释',content='天气好啊')
-
+    # blog = Blog(name='新浪微博', user_id= userID , user_name= userName, user_image= userImage, id=next_id(),
+    #             created_at=time.time(),summary='我是一个注释',content='天气好啊')
+    #
+    # insert user table
     await  user.save()
-    await blog.save()
+    # await  blog.save()
+    #select table
+    blog =  await Blog.find('0015096735833925892090c774a4f108fe149f95bbef726000')
+    print(blog.id , blog.user_name)
+
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(test(loop))
